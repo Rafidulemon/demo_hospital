5c8826d2808cd32e40bcfde6824c78b9a6424aa7
Md Rafidul Islam <rafidulemon@gmail.com>
2025-02-07 17:27:44 +0600

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import { FaPhoneVolume } from "react-icons/fa6";
 
 export const metadata: Metadata = {
   title: "Hospital",
   description: "Patiya Hospital",
   icons: {
     icon: "/logo/icon.png",
-  }
+  },
 };
 
 export default async function RootLayout({
@@ -22,9 +23,19 @@ export default async function RootLayout({
       <head>
         <link rel="icon" href="/logo/icon.png" type="image/png" />
       </head>
-      <body className={`antialiased bg-white flex flex-col gap-0`}>
+      <body className="antialiased bg-white flex flex-col gap-0">
         <NextIntlClientProvider messages={messages}>
           <div className="min-h-[calc(80vh-10px)]">{children}</div>
+          <a
+            href="tel:+1234567890"
+            className="fixed bottom-4 right-4 bg-gray-300 text-primary pr-2 rounded-full shadow-lg flex items-center justify-center transition-all duration-300 hover:scale-110"
+          >
+            <div className="z-10 -ml-2 shadow-md bg-primary text-white p-2 rounded-full">
+              <FaPhoneVolume className="text-xl" />
+            </div>
+
+            <span className="px-2">+1234567890</span>
+          </a>
         </NextIntlClientProvider>
       </body>
     </html>
--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -45,9 +45,9 @@ export default function Header() {
             </span>
             <div className="group cursor-pointer text-[10px] md:text-[16px] hover:bg-primary border border-primary rounded-md flex flex-row items-center gap-[3px] md:gap-2 py-1 md:py-2 px-1 md:px-3">
               <FaPhoneAlt className="text-primary group-hover:text-white" />
-              <span className="text-black text-[12px] md:text-[14px] font-medium group-hover:text-white">
+              <a href="tel:12345" className="text-black text-[12px] md:text-[14px] font-medium group-hover:text-white">
                 12345
-              </span>
+              </a>
             </div>
           </div>
 
@@ -57,9 +57,9 @@ export default function Header() {
             </span>
             <div className="group cursor-pointer text-[10px] md:text-[16px] hover:bg-primary border border-primary rounded-md flex flex-row items-center gap-[3px] md:gap-2 py-1 md:py-2 px-1 md:px-3">
               <FaPhoneAlt className="text-primary group-hover:text-white" />
-              <span className="text-black text-[12px] md:text-[14px] font-medium group-hover:text-white">
+              <a href="tel:+8801365412345" className="text-black text-[12px] md:text-[14px] font-medium group-hover:text-white">
                 +8801365412345
-              </span>
+              </a>
             </div>
           </div>
 
